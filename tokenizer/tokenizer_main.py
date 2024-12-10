41656bba099cc0f9220e3c788fdf4409f1a58c15
changgyu Boo <79037397+genji970@users.noreply.github.com>
2024-12-11 01:52:11 +0900

    Delete tokenizer directory
    Signed-off-by: changgyu Boo <79037397+genji970@users.noreply.github.com>

--- tokenizer/tokenizer_main.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-import ray
-
-from data_structure_build import *
-from tokenizing_process import *
-from master.config import *
-
-tokenized_data = tokenizing(data_preprocess(config))
