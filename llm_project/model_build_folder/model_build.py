dc71b470701246ae5399efc51a30f2b3042da771
changgyu Boo <79037397+genji970@users.noreply.github.com>
2024-12-11 01:56:24 +0900

    Add files via upload
    Signed-off-by: changgyu Boo <79037397+genji970@users.noreply.github.com>

---
+++ llm_project/model_build_folder/model_build.py
@@ -0,0 +1,38 @@
+import os
+import warnings
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+#custome import
+from model_build_folder.lora import lora_config
+from model_build_folder.model_get import load_model , model_freeze , model_ready
+
+#Random seed fixed
+import random
+
+seed = 40
+deterministic = True
+
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+if deterministic:
+  torch.backends.cudnn.deterministic = True
+  torch.backends.cudnn.benchmark = False
+warnings.filterwarnings('ignore')
+
+
+#Lora_configuration
+lora_config = lora_config
+
+# model_init
+base_model , model_name = load_model()
+
+model_freeze(base_model)
+
+llm_model , tokenizer = model_ready(base_model , model_name)
+
